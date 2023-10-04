1bfde86b40dc5b8914a0f0e4d265e1674898c8f7
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2023-10-04 11:42:34 -0600

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -22,6 +22,7 @@ if np.any(canvas_result.image_data):
     # Display prediction
     st.header('Prediction:')
     st.markdown('This number appears to be a \n # :red[' + str(prediction) + ']')
+    st.balloons()
 else:
     # Display message if canvas is empty
     st.header('Prediction:')
