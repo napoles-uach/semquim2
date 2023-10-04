774e9a4048a5e9f39a39ffd0d4af474a286a35b6
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2023-10-04 02:21:48 +0000

    fin

--- Hello.py
+++ Hello.py
@@ -9,24 +9,12 @@ st.markdown("# Digit :blue[Recognition] :green[App] :pencil: 💻🤖")
 # Load trained model
 model = tf.keras.models.load_model('mi_modelo.h5')
 
-#def process_image(image_data, size=28):
-#  """Convert drawn image to grayscale and resize to 28x28."""
-#  # Convert image to grayscale
-#  grayscale_image = np.sum(image_data, axis=2)
-#  # Resize image
-#  resized_image = zoom(grayscale_image, size / grayscale_image.shape[0])
-#  # Normalize pixel values
-#  normalized_image = resized_image.astype(np.float32) / 255
-#  # Return image as a single row
-#  return normalized_image.reshape(1, -1)
-
 st.write('Draw a digit:')
 # Display canvas for drawing
 canvas_result = st_canvas(stroke_width=10, height=28*5, width=28*5)
   
 # Process drawn image and make prediction using model
 if np.any(canvas_result.image_data):
-    #st.write(canvas_result.image_data)
     # Convert drawn image to grayscale and resize to 28x28
     processed_image = process_image(canvas_result.image_data)
     # Make prediction using model
